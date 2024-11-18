b627ea601818893cb1693421df1765a961271d80
Bilius10 <jvdrdo@gmail.com>
2024-11-17 21:15:19 -0300

    Adicionando pasta1 e pasta2

---
+++ Gerenciar/src/test/java/com/boletos/Gerenciar/GerenciarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boletos.Gerenciar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
