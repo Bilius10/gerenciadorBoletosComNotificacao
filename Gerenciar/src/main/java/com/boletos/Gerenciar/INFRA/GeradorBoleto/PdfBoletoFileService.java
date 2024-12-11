af7f7072a235e9bfdc51ff3fdeb379a37cf72623
Bilius10 <jvdrdo@gmail.com>
2024-12-10 21:42:17 -0300

    Terminei toda a criação do boleto

--- Gerenciar/src/main/java/com/boletos/Gerenciar/INFRA/GeradorBoleto/PdfBoletoFileService.java
+++ Gerenciar/src/main/java/com/boletos/Gerenciar/INFRA/GeradorBoleto/PdfBoletoFileService.java
@@ -2,19 +2,36 @@ package com.boletos.Gerenciar.INFRA.GeradorBoleto;
 
 import br.com.caelum.stella.boleto.transformer.GeradorDeBoleto;
 import com.boletos.Gerenciar.ENTITY.GeradorBoleto.Fatura;
+import com.boletos.Gerenciar.REPOSITORY.FaturaRegistradaRepository;
 import com.boletos.Gerenciar.SERVICE.GeradorBoleto.GeradorBoletoService;
+import com.boletos.Gerenciar.SERVICE.GeradorQrCode.GeradorQrCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class PdfBoletoFileService implements GeradorBoletoService {
 
+    @Autowired
+    private GeradorQrCodeService geradorQrCode;
+    @Autowired
+    private FaturaRegistradaRepository faturaRegistradaRepository;
 
     @Override
     public byte[] gerar(Fatura fatura, CobrancaInput cobranca) {
 
         var boleto = criarBoleto(fatura, cobranca);
 
-        var gerador = new GeradorDeBoleto(boleto);
+        var pathBoletoPersonalizado = this.getClass().getResourceAsStream("/Relatorios/Boleto-Personalizado.jasper");
+        var pathLogo = this.getClass().getResourceAsStream("/Relatorios/img.png");
+        var faturaRegistrada  = faturaRegistradaRepository.buscarPorFaturaId(fatura.getIdFatura());
+
+        var parametros = new HashMap<String, Object>();
+        parametros.put("QRCODE", geradorQrCode.gerar(faturaRegistrada.getQrcodeEmv()));
+        parametros.put("LOGO", pathLogo);
+
+        var gerador = new GeradorDeBoleto(pathBoletoPersonalizado, parametros, boleto);
 
         return gerador.geraPDF();
     }
--- Gerenciar/src/main/java/com/boletos/Gerenciar/REPOSITORY/FaturaRegistradaRepository.java
+++ Gerenciar/src/main/java/com/boletos/Gerenciar/REPOSITORY/FaturaRegistradaRepository.java
@@ -2,8 +2,12 @@ package com.boletos.Gerenciar.REPOSITORY;
 
 import com.boletos.Gerenciar.ENTITY.GeradorBoleto.FaturaRegistrada;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FaturaRegistradaRepository extends JpaRepository<FaturaRegistrada, Long> {
+
+    @Query("SELECT f FROM FaturaRegistrada f WHERE f.fatura.idFatura = :faturaId")
+    FaturaRegistrada buscarPorFaturaId(int faturaId);
 }
